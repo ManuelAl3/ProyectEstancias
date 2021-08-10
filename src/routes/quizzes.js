c07cebe63e76e7787a8cfe4eed21603dda90510d
Joel Madrid <andres.javm00@gmail.com>
2021-08-09 19:24:22 -0500

    Se logro ingresar datos a la tabla Respuestas

--- src/routes/quizzes.js
+++ src/routes/quizzes.js
@@ -8,8 +8,24 @@ router.get('/add', (req, res) => {
     res.render('quizzes/add');
 });
 
-router.post('/add', (req, res) => {
-    res.send('received');
+router.post('/add', async (req, res) => {
+    //console.log(req);
+    const data = req.body;
+    console.log(data);
+    let result;
+    const sql = "INSERT INTO answers (`answer`,`user_id`,`option_id`,`question_id`) VALUES (?,?,?,?)"
+    for(i = 0; i < data.question_id.length; i++){
+        let answer = data.answer[i];
+        let user_id = data.user_id[i];
+        let option_id = data.option_id[i] ? data.option_id[i] : null;
+        let question_id = data.question_id[i];
+        result = await pool.query(sql, [answer, user_id, option_id, question_id]);
+    };
+    
+    //res.send('received');
+    res.json(result);
 });
+
+
 
 module.exports = router;
